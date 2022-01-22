e5bd89e8c28a487f2bd34f04c8bab47fc180c22d
Johnson Che <Johnson.Che01@gmail.com>
2022-01-22 15:34:59 -0500

    adds styling to nav

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Hero.module.css';
+
+function Hero() {
+  return (
+    <div className={styles.hero}>
+      <h1>GQLevated</h1>
+    </div>
+  );
+}
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Nav from '../components/Nav';
 import styles from '../styles/Layout.module.css';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Hero />
     </>
   );
 }
