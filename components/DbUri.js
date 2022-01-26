c7216e7b7770a0c78d8ea17a174b42a68a0c07b8
Johnson Che <Johnson.Che01@gmail.com>
2022-01-26 12:14:22 -0500

    setup connection to backend

--- components/DbUri.js
+++ components/DbUri.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import styles from '../styles/DbUri.module.css';
 import { Input, Spacer, Button, Loading } from '@nextui-org/react';
+import CryptoJS from 'crypto-js';
+import secretKey from '../server/secretKey';
 
 const easing = [0.83, 0, 0.17, 1];
 
@@ -26,33 +28,51 @@ const fadeInRight = {
   },
 };
 
-const fadeInLeft = {
-  initial: {
-    x: [0, 50, -400],
-    opacity: 1,
-  },
-  animate: {
-    x: 0,
-    opacity: 0,
-    transition: {
-      delay: 10,
-      type: 'spring',
-      duration: 1.0,
-      // ease: easing,
-    },
-  },
-};
-
 function DbUri({ hidePanel }) {
+  const uriField = useRef();
+  const [errorMsg, setErrorMsg] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const handleClick = (e) => {
+    const URILink = uriField.current.value;
+    e.preventDefault();
+    if (URILink.length > 0) {
+      const encryptedURL = CryptoJS.AES.encrypt(URILink, secretKey).toString();
+
+      fetch('http://localhost:8080/convert-sql-db', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ link: encryptedURL }),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => {
+          setErrorMsg(err);
+          setIsError(true);
+        });
+    }
+  };
+
   return (
     <AnimatePresence exitBeforeEnter>
       <motion.div variants={fadeInRight} initial='initial' exit='exit' animate='initial' className={styles.container}>
         <div className={styles.uriString}>
           <h2>Elevate your project with seamless integration</h2>
           <Spacer y={1.5} />
-          <Input clearable bordered width='20rem' labelPlaceholder='PostgreSQL URI' initialValue='' />
+          {isError && <span>{errorMsg.err}</span>}
+          <Input
+            clearable
+            bordered
+            width='20rem'
+            labelPlaceholder='PostgreSQL URI'
+            initialValue=''
+            ref={uriField}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') handleClick(e);
+            }}
+          />
           <Spacer y={1.5} />
-          <Button auto clickable={true} color='gradient' rounded='false' size='sm' css={{ px: '$14' }} onClick={() => hidePanel()}>
+          <Button auto clickable={true} color='gradient' rounded='false' size='sm' css={{ px: '$14' }} onClick={(e) => handleClick(e)}>
             Submit
           </Button>
         </div>
--- next.config.js
+++ next.config.js
@@ -5,3 +5,26 @@ const withTM = require('next-transpile-modules')(['react-github-btn']);
 // };
 
 module.exports = withTM();
+
+// module.exports = {
+//   withTM: withTM(),
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: '/convert-sql-db',
+//         destination: 'http://localhost:8080/convert-sql-db', // Proxy to Backend
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/convert-sql-db',
+//         destination: 'http://localhost:8080/convert-sql-db', // Proxy to Backend
+//       },
+//     ];
+//   },
+// };
